1c22ccd6f97f3de444d6f9b31c31332ec28bde8d
Abdul Hakim <hakim@undercurrent.tech>
2024-07-05 11:55:21 +0700

    handle burn failed

--- src/private.rs
+++ src/private.rs
@@ -4,29 +4,34 @@ use crate::*;
 impl Contract {
   #[private]
   pub fn resolve_burn(&mut self, token_id: AccountId, account_id: AccountId, amount: u128) {
-    let session_key = SessionKey(token_id, account_id);
-    let mut account = if let Some(account) = self.accounts.get(&session_key) {
-        account.clone()
-    } else {
-        Account { burn_amount: 0, session: 0 }
-    };
+    require!(env::promise_results_count() == 1);
 
-    if let Some(new_balance) = account.burn_amount.checked_add(amount) {
-        account.burn_amount = new_balance;
-    } else {
-        env::panic_str("Overflow account amount");
-    }
-
-    if let Some(new_session) = amount.checked_div(u128::pow(10, 18)) {
-        if let Some(total_session) = account.session.checked_add(new_session) {
-            account.session = total_session;
+    if let PromiseResult::Successful(_) = env::promise_result(0) {
+        let session_key = SessionKey(token_id, account_id);
+        let mut account = if let Some(account) = self.accounts.get(&session_key) {
+            account.clone()
         } else {
-            env::panic_str("Overflow account session");
+            Account { burn_amount: 0, session: 0 }
+        };
+    
+        if let Some(new_balance) = account.burn_amount.checked_add(amount) {
+            account.burn_amount = new_balance;
+        } else {
+            env::panic_str("Overflow account amount");
         }
-    } else {
-        env::panic_str("Invalid division");
+    
+        if let Some(new_session) = amount.checked_div(u128::pow(10, 18)) {
+            if let Some(total_session) = account.session.checked_add(new_session) {
+                account.session = total_session;
+            } else {
+                env::panic_str("Overflow account session");
+            }
+        } else {
+            env::panic_str("Invalid division");
+        }
+    
+        self.accounts.insert(session_key, account);
     }
-
-    self.accounts.insert(session_key, account);
+    
   }
 }
